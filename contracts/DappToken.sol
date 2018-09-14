232063432ef93536164fdbc93146627b55a95c24
seyeadama03 <seyeadama03@outlook.com>
2018-09-14 16:36:28 +0000

    creating token erc-20

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,10 +1,19 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.2;
 
 contract  DappToken{
    //constructor
-      uint256 public totalSupply;
-     constructor () public{
-       totalSupply = 1000000;
-   }
-
+   //name 
+   string public name = "Dapp Token";
+   //symbol
+   string public symbol = "DAPP";
+   //standard
+   string public standard = "DApp Token v1.0";
+   uint256 public totalSupply;
+   mapping(address =>uint256) public balanceOf;
+   constructor(uint256 _initialSupply) public{
+       balanceOf[msg.sender]=_initialSupply;
+       totalSupply = _initialSupply;
+       //alocate the balance
+     }
+    
 }
